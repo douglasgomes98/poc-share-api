49e4e6ea166b6180d1fd18c49cac2b0ab428247d
Douglas Gomes <douglasgomes.rr@gmail.com>
2021-10-14 19:58:50 -0400

    update share method

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const shareButton = document.querySelector("button");
 
-const shareData = {
-  title: "MDN",
-  text: "Aprenda desenvolvimento web no MDN!",
-  url: "https://developer.mozilla.org",
-};
-
-shareButton.addEventListener("click", async () => {
-  await navigator.share(shareData);
+shareButton.addEventListener("click", () => {
+  if (navigator.share) {
+    navigator
+      .share({
+        title: "Web Share API",
+        text: "Sent with the Web Share API",
+        url: "https://github.com/douglasgomes98/poc-share-api",
+      })
+      .then(() => {
+        console.log("Shared successfully.");
+      })
+      .catch((error) => {
+        console.log("There was an error sharing:", error);
+      });
+  } else {
+    console.log("The Web Share API is not supported in your browser.");
+  }
 });
