4959211c90a25601c19e2310db376e9c68c30f30
Douglas Gomes <douglasgomes.rr@gmail.com>
2021-10-14 20:27:18 -0400

    feat: add slice image

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-const shareButton = document.querySelector("button");
+import sliceImage from "./slice.png";
+
+const shareTextButton = document.querySelector("#button-text");
+const shareImageButton = document.querySelector("#button-img");
 
 const textareaText = document.querySelector("textarea");
 
-shareButton.addEventListener("click", () => {
+shareTextButton.addEventListener("click", () => {
   if (navigator.share) {
     navigator
       .share({
         title: "Web Share API",
         text: textareaText.value,
+      })
+      .then(() => {
+        console.log("Shared successfully.");
+      })
+      .catch((error) => {
+        console.log("There was an error sharing:", error);
+      });
+  } else {
+    console.log("The Web Share API is not supported in your browser.");
+  }
+});
+
+shareImageButton.addEventListener("click", () => {
+  if (navigator.share) {
+    navigator
+      .share({
+        title: "Web Share API",
+        files: [sliceImage],
       })
       .then(() => {
         console.log("Shared successfully.");
